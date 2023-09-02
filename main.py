34373048143b1000017ccbf97b0f2c055f1103f8
Mausmato <92238695+Mausmato@users.noreply.github.com>
2023-09-01 22:07:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import streamlit as sl
 
+
+sl.markdown("""
+‹style>
+.css-1wbqy5l e17vllj40 {
+  visibility: hidden;
+  
+  
+}
+
+.css-cio0dv ea3mdgi1 {
+  
+  visibility: hidden
+  
+}
+</style>
+""", unsafe_allow_html=True)
+
 sl.title("FOOTPRINT")
 sl.button("Calculate Now")
 sl.subheader("Our Mission")
