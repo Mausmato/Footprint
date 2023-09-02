a87da0e3b543c03f18e145dd33748afe480a92df
ethan xing <ethanxing2007@gmail.com>
2023-09-02 01:11:43 -0400

    main to home

--- Home.py
+++ Home.py
@@ -12,4 +12,3 @@ sl.text("Click here to track your FOOTPRINT")
 sl.subheader("Our Mission")
 sl.markdown("  Here at **FOOTPRINT** we strive to make an eco-friendly environment that is both accessible and practical. We have created a platform where users can track and calculate their **carbon footprints** to see exactly how much of an impact they have on our lovley planet. After all, a little goes a long way.")
 
-
