e1ca345c3ffea0b9b0702322493cd08519197768
Mausmato <92238695+Mausmato@users.noreply.github.com>
2023-09-01 22:40:56 -0400

    FINALLY UPDATED THE CSS!!

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as sl
 with open('style.css') as f:
-  sl.markdown(f'<style>(f.read()</style>', unsafe_allow_html=True)
+  sl.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
   
 sl.title("Hello, welcome to FOOTPRINT.")
 
@@ -8,3 +8,5 @@ sl.button("Calculate Now")
 sl.text("Click here to track your FOOTPRINT")
 sl.subheader("Our Mission")
 sl.markdown("Here at **FOOTPRINT** we strive to make an eco-friendly environment that is both accessible and practical. ")
+
+
