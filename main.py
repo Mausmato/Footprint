49980897263ead9bb75caa263829f2446a7efc7d
Mausmato <92238695+Mausmato@users.noreply.github.com>
2023-09-01 22:36:35 -0400

    New Css
    yay

--- main.py
+++ main.py
@@ -3,18 +3,6 @@ import streamlit as sl
 with open('style.css') as f:
   sl.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-sl.markdown("""
-‹style>
-.css-cio0dv.ea3mdgi1
-{
-  visibility: hidden;
-}
-.css-czk5ss.e16jpq800
-{
-  visibility: hidden
-}
-</style>
-""", unsafe_allow_html=True)
 
 sl.title("FOOTPRINT")
 sl.button("Calculate Now")
