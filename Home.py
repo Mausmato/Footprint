dac57b6e1025ca3fa9c0b56f6dfba143980d82cb
ethan xing <ethanxing2007@gmail.com>
2023-09-02 01:50:09 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ with open('style.css') as f:
 sl.title("Hello, welcome to FOOTPRINT.")
 
 sl.button("Calculate Now")
-sl.text("Click here to track your FOOTPRINT")
+sl.text("Click here to track your FOOTPRINT with our curated survey.")
 sl.subheader("Our Mission")
 sl.markdown("  Here at **FOOTPRINT** we strive to make an eco-friendly environment that is both accessible and practical. We have created a platform where users can track and calculate their **carbon footprints** to see exactly how much of an impact they have on our lovley planet. After all, a little goes a long way.")
 
