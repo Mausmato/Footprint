70ef359a39315899755f353e4365433dc3f6f07f
ethan xing <ethanxing2007@gmail.com>
2023-09-02 01:01:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as sl
+
 sl.sidebar.title("Navigation")
 
 with open('style.css') as f:
@@ -9,6 +10,6 @@ sl.title("Hello, welcome to FOOTPRINT.")
 sl.button("Calculate Now")
 sl.text("Click here to track your FOOTPRINT")
 sl.subheader("Our Mission")
-sl.markdown("Here at **FOOTPRINT** we strive to make an eco-friendly environment that is both accessible and practical. ")
+sl.markdown("  Here at **FOOTPRINT** we strive to make an eco-friendly environment that is both accessible and practical. We have created a platform where users can track and calculate their **carbon footprints** to see exactly how much of an impact they have on our lovley planet. After all, a little goes a long way.")
 
 
