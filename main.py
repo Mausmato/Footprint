1ff6a89a8734787b158e04009d6359ab5452968b
ethan xing <ethanxing2007@gmail.com>
2023-09-01 22:35:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import streamlit as sl
 
-with open('style.css') as f:
-  sl.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 sl.markdown("""
 ‹style>
-.css-cio0dv.ea3mdgi1
-{
+.css-1wbqy5l e17vllj40 {
   visibility: hidden;
+  
+  
 }
-.css-czk5ss.e16jpq800
-{
+
+.css-cio0dv ea3mdgi1 {
+  
   visibility: hidden
+  
 }
 </style>
 """, unsafe_allow_html=True)
 
-sl.title("FOOTPRINT")
-sl.button("Calculate Now")
-sl.subheader("Our Mission")
-sl.markdown("")
+sl.title("Hello, welcome to FOOTPRINT.")
 
+sl.button("Calculate Now")
+sl.text("Click here to track your FOOTPRINT")
+sl.subheader("Our Mission")
+sl.markdown("Here at **FOOTPRINT** we strive to make an eco-friendly environment that is both accessible and practical. ")
+with open('style.css') as f:
+  sl.markdown(f'<style>(f.read()</style>', unsafe_allow_html=True)
