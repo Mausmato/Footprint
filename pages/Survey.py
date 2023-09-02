8f613e3a4d12a047ed3d82cccca9ce42cf5bd59a
JLi2007 <lij9532@wrdsb.ca>
2023-09-02 02:05:30 -0400

    Update Survey.py

--- pages/Survey.py
+++ pages/Survey.py
@@ -13,7 +13,7 @@ commital = labels.index(sl.select_slider('How committed are you to emit less Car
 
 q2 = sl.slider('How many kilometres do you drive on average a week', 0, 5000, format = '%.0fkm')
 
-q20 = sl.slider('How much of the products you shop for are second-hand or sustainable products??',0, 100,format="%.0f%")
+q20 = sl.slider('How much of the products you shop for are second-hand or sustainable products??',0, 100,format="%.0f%%")
 
 q21 = labels.index(sl.select_slider('How often do you buy new clothes per year?', options=labels))
 
