300b2aeb5f817ab5af095620bd61a5b693c9677a
JLi2007 <lij9532@wrdsb.ca>
2023-09-03 01:18:44 -0400

    Update todo.py

--- pages/todo.py
+++ pages/todo.py
@@ -3,6 +3,8 @@ from streamlit.components.v1 import html
 import pandas as pd
 import altair as alt
 import numpy as py
+import pandas as pd
+from datetime import datetime
 
 sl.set_page_config(page_title='FOOTPRINT')
 sl.sidebar.title("Navigation")
@@ -12,34 +14,54 @@ with open('style.css') as f:
   sl.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
   
 sl.title("Hello, welcome to FOOTPRINT.")
+
 # Create a title for your app
-sl.title('To-Do List App')
+sl.title('Advanced To-Do List App')
 
 # Initialize tasks in the session state
 if 'tasks' not in sl.session_state:
-    sl.session_state.tasks = []
+    sl.session_state.tasks = pd.DataFrame(columns=['Task', 'Priority', 'Due Date'])
 
-# Create an input field for adding tasks
+# Create input fields for adding tasks
 task = sl.text_input('Add a new task:', '')
+priority = sl.selectbox('Priority:', ['Low', 'Medium', 'High'])
+due_date = sl.date_input('Due Date:', datetime.today())
 
 # Add a button to add tasks to the list
 if sl.button('Add Task'):
     if task:
-        sl.session_state.tasks.append({"task": task, "completed": False})
+        new_task = {'Task': task, 'Priority': priority, 'Due Date': due_date}
+        sl.session_state.tasks = pd.concat([sl.session_state.tasks, pd.DataFrame([new_task])], ignore_index=True)
         task = ''
+        priority = 'Low'
+        due_date = datetime.today()
 
-# Display the tasks
+# Create task filtering options
+sl.sidebar.header('Task Filters')
+selected_priority = sl.sidebar.selectbox('Select Priority:', ['All', 'Low', 'Medium', 'High'])
+show_completed = sl.sidebar.checkbox('Show Completed Tasks')
+
+# Filter and display tasks
+filtered_tasks = sl.session_state.tasks.copy()
+
+if selected_priority != 'All':
+    filtered_tasks = filtered_tasks[filtered_tasks['Priority'] == selected_priority]
+
+if not show_completed:
+    filtered_tasks = filtered_tasks[~filtered_tasks['Task'].str.startswith('[Completed]')]
+
 sl.subheader('Tasks:')
-for i, task_info in enumerate(sl.session_state.tasks):
-    task = task_info["task"]
-    completed = task_info["completed"]
+for i, row in filtered_tasks.iterrows():
+    task = row['Task']
+    priority = row['Priority']
+    due_date = row['Due Date'].strftime('%Y-%m-%d')
+    task_label = f"{i + 1}. {task} (Priority: {priority}, Due Date: {due_date})"
+    completed = sl.checkbox(task_label, False)
     
-    # Create a checkbox to mark tasks as completed
-    completed = sl.checkbox(f"{i + 1}. {task}", completed)
-    
-    # Update the completed status in the session state
-    sl.session_state.tasks[i]["completed"] = completed
+    # Update the task status in the session state
+    if completed:
+        sl.session_state.tasks.loc[i, 'Task'] = f"[Completed] {task}"
 
 # Add an option to clear completed tasks
 if sl.button('Clear Completed Tasks'):
-    sl.session_state.tasks = [task_info for task_info in sl.session_state.tasks if not task_info["completed"]]
+    sl.session_state.tasks = filtered_tasks[~filtered_tasks['Task'].str.startswith('[Completed]')]
