6307cb377dd8d7bb290f3910b795e677f1a4eef6
Alex <alex.rusu0720@gmail.com>
2023-09-03 09:59:35 -0400

    .

--- pages/1_Survey_Home.py
+++ pages/1_Survey_Home.py
@@ -7,7 +7,7 @@ sl.sidebar.image("assets/footprint.png")
 with open('style.css') as f:
   sl.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-sl.title('CARBON FOOTPRING Calculator')
+sl.title('CARBON FOOTPRINT Calculator')
 sl.subheader('5 minute quiz')
 
 if sl.button('Next', on_click = sl.write()):
