8b54195226ce1630057370fdba5c0bc867e56999
Mausmato <92238695+Mausmato@users.noreply.github.com>
2023-09-02 21:58:51 -0400

    Dash1 push

--- pages/dash1.py
+++ pages/dash1.py
@@ -1,15 +1,24 @@
-import streamlit as sl
+import streamlit as st
 
-sl.sidebar.title("Navigation")
-sl.sidebar.image("assets/footprint.png")
+st.sidebar.title("Navigation")
+st.sidebar.image("assets/footprint.png")
 
 with open('style.css') as f:
-  sl.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-  
-sl.title("Hello, welcome to your **DASHBOARD**.")
-sl.subheader("Three Main Factors")
-col1, col2, col3 = sl.columns(3)
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-col1.write("this is column1")
-col2.write("this is column2")
-col3.write("this is column3")
+st.title("Hello, welcome to your **DASHBOARD**.")
+st.subheader("Three Main Factors")
+col1, col2, col3 = st.columns(3)
+
+# Insert HTML content in col1
+
+# Create a link in col3
+col1.header("Recommendations")
+col1.write("Visit the [Recommendations] Page (http://localhost:8501/Dashboard)")
+# Create a link in col2
+col2.header("To Do")
+col2.write("Visit [ToDo](https://www.google.com)")
+
+# Create a link in col3
+col3.header("Strategy")
+col3.write("Visit the [Strategy] Page (http://localhost:8501/Strategy)")
