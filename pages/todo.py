06b7bfd8233d0ef41bc6a6b8b5cf273133094c85
JLi2007 <lij9532@wrdsb.ca>
2023-09-03 00:17:46 -0400

    Update todo.py

--- pages/todo.py
+++ pages/todo.py
@@ -12,37 +12,34 @@ with open('style.css') as f:
   sl.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
   
 sl.title("Hello, welcome to FOOTPRINT.")
-
-import streamlit as st
-
 # Create a title for your app
-st.title('To-Do List App')
+sl.title('To-Do List App')
 
 # Initialize tasks in the session state
-if 'tasks' not in st.session_state:
-    st.session_state.tasks = []
+if 'tasks' not in sl.session_state:
+    sl.session_state.tasks = []
 
 # Create an input field for adding tasks
-task = st.text_input('Add a new task:', '')
+task = sl.text_input('Add a new task:', '')
 
 # Add a button to add tasks to the list
-if st.button('Add Task'):
+if sl.button('Add Task'):
     if task:
-        st.session_state.tasks.append({"task": task, "completed": False})
+        sl.session_state.tasks.append({"task": task, "completed": False})
         task = ''
 
 # Display the tasks
-st.subheader('Tasks:')
-for i, task_info in enumerate(st.session_state.tasks):
+sl.subheader('Tasks:')
+for i, task_info in enumerate(sl.session_state.tasks):
     task = task_info["task"]
     completed = task_info["completed"]
     
     # Create a checkbox to mark tasks as completed
-    completed = st.checkbox(f"{i + 1}. {task}", completed)
+    completed = sl.checkbox(f"{i + 1}. {task}", completed)
     
     # Update the completed status in the session state
-    st.session_state.tasks[i]["completed"] = completed
+    sl.session_state.tasks[i]["completed"] = completed
 
 # Add an option to clear completed tasks
-if st.button('Clear Completed Tasks'):
-    st.session_state.tasks = [task_info for task_info in st.session_state.tasks if not task_info["completed"]]
+if sl.button('Clear Completed Tasks'):
+    sl.session_state.tasks = [task_info for task_info in sl.session_state.tasks if not task_info["completed"]]
