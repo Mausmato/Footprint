1d8760a3183c271201801ed07b329a4b5bec325c
Mausmato <92238695+Mausmato@users.noreply.github.com>
2023-09-02 21:45:03 -0400

    ADdding MAIN DASH PAGE

---
+++ pages/dash1.py
@@ -0,0 +1,15 @@
+import streamlit as sl
+
+sl.sidebar.title("Navigation")
+sl.sidebar.image("assets/footprint.png")
+
+with open('styles.css') as f:
+  sl.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+  
+sl.title("Hello, welcome to your **DASHBOARD**.")
+sl.subheader("Three Main Factors")
+col1, col2, col3 = sl.columns(3)
+
+col1.write("this is column1")
+col2.write("this is column2")
+col3.write("this is column3")
